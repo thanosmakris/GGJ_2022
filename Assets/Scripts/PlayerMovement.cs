2ea59415d6353e0ff48ee876a13ef46cf11de59d
Thanos Makris <thanosmak123@gmail.com>
2022-01-22 15:10:55 +0200

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float rotationSpeed;
+    public Transform rotationParent;
+    
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Movement();
+        Rotation();
+    }
+
+    void Movement()
+    {
+        float hor = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float ver = Input.GetAxis("Vertical") * Time.deltaTime;
+        Vector3 direction = new Vector3(hor, 0f, ver);
+
+        transform.position += direction * speed;
+    }
+
+    void Rotation()
+    {
+        if (Input.GetKey(KeyCode.Q)) // rotate LEFT
+        {
+            rotationParent.Rotate(Vector3.up * Time.deltaTime * -rotationSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.E)) // rotate RIGHT
+        {
+            rotationParent.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        }
+    }
+}
