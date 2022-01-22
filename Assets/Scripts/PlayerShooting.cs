905fad4bf551308b9db1ea97018a9b3ee2248240
Thanos Makris <thanosmak123@gmail.com>
2022-01-22 15:37:50 +0200

    weapon swap

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,18 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject weaponPrefab;
+    public Transform[] weaponSlots;
+    Transform weaponTransform;
+    Queue<Transform> slotsQueue = new Queue<Transform>();
+
+
+    private void OnEnable() {
+        GameObject weaponInstance = Instantiate(weaponPrefab, weaponSlots[0].position, Quaternion.identity);
+        weaponTransform = weaponInstance.transform;
     }
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        slotsQueue.Enqueue(weaponSlots[0]);
+        slotsQueue.Enqueue(weaponSlots[1]);
+    }
+
+
     void Update()
     {
-        
+        SwapPositions();
+        Fire();
+    }
+
+
+    void SwapPositions()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Transform newPos = slotsQueue.Dequeue();
+            slotsQueue.Enqueue(newPos);
+            weaponTransform.position = newPos.position;
+            weaponTransform.parent = newPos;
+        }
+    }
+
+    void Fire()
+    {
+
     }
 }
